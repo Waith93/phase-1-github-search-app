7efd6372767b5740fe470d221680ab43ce0c7112
josephbill <josephbill00@gmail.com>
2022-03-01 14:51:23 +0300

    completed assignments

---
+++ js/index.js
@@ -0,0 +1,19 @@
+let form = document.getElementById('github-form')
+form.addEventListener('submit', function (e){
+    e.preventDefault();
+    let search = document.getElementById("search").value
+    let searchClean = search.split(' ').join('')
+    fetch("https://api.github.com/users/"+searchClean).then((result) => result.json())
+        .then((data) => {
+
+               document.getElementById("results").innerHTML =
+                `<div>` +
+                `<a target="_blank" href="https://www.github.com/${searchClean}"> <img src="${data.avatar_url}" /></a>`+
+                `</div>` +
+                `<div>
+                 <a target="_blank" href="https://www.github.com/${searchClean}"> <p style="padding: 10px">${data.login}</p> </a>
+                 </div>`
+
+        })
+
+})
