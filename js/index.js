2e0c2926be2e57f82a7f9b6fa7342e7918ac3f83
josephbill <josephbill00@gmail.com>
2025-03-20 11:19:20 +0300

    handling data fetches from external apis

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
-let form = document.getElementById('github-form')
-form.addEventListener('submit', function (e){
-    e.preventDefault();
-    let search = document.getElementById("search").value
-    let searchClean = search.split(' ').join('')
-    fetch("https://api.github.com/users/"+searchClean).then((result) => result.json())
-        .then((data) => {
+document.addEventListener("DOMContentLoaded", function (){
+    //selector to capture form 
+    let form = document.getElementById('github-form')
 
-               document.getElementById("results").innerHTML =
-                `<div>` +
-                `<a target="_blank" href="https://www.github.com/${searchClean}"> <img src="${data.avatar_url}" /></a>`+
-                `</div>` +
-                `<div>
-                 <a target="_blank" href="https://www.github.com/${searchClean}"> <p style="padding: 10px">${data.login}</p> </a>
-                 </div>`
+    form.addEventListener('submit', function (e){
+        e.preventDefault();
+        //search input selector 
+        let search = document.getElementById("search").value
+        // clean the input 
+        let searchClean = search.split(' ').join('')
+        
+        fetch("https://api.github.com/users/"+searchClean)
+            .then((result) => result.json())
+            .then((data) => {
+    
+                   document.getElementById("results").innerHTML =
+                    `<div>` +
+                    `<a target="_blank" href="https://www.github.com/${searchClean}"> <img src="${data.avatar_url}" /></a>`+
+                    `</div>` +
+                    `<div>
+                     <a target="_blank" href="https://www.github.com/${searchClean}"> <p style="padding: 10px">${data.login}</p> </a>
+                     </div>`
 
-        })
+                    
+
+            })
+    
+    })
 
 })
